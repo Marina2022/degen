734d4a45a6c9d047f12f91eca0d48ab6301fdf3a
Marina2022 <marusiiiia@yandex.ru>
2024-05-24 12:51:52 +0300

    mobile-fix3

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -3,27 +3,30 @@ import logo from '@/assets/img/header/headerLogo.png'
 import twitterIcon from '@/assets/img/header/socialIcons/Twitter.svg'
 import telegramIcon from '@/assets/img/header/socialIcons/Telegram.svg'
 import LangSwitch from "@/components/ui/LangSwitch/LangSwitch.jsx";
+
 const Header = ({lang, setLang}) => {
   return (
       <header className={s.header}>
         <div className={s.logo}>
           <img src={logo} alt="logo"/>
-          <span>$DEGEN</span>          
+          <span>$DEGEN</span>
         </div>
 
         <div className={s.rightHeaderMenu}>
           <a className={s.socialLink} href="https://x.com/TonDegeneration" target="_blank">
             <img src={twitterIcon} alt="twitter"/>
-            
+
           </a>
           <a className={s.socialLink} href="https://t.me/TonDegeneration" target="_blank">
-            <img src={telegramIcon} alt="telegram"/>            
+            <img src={telegramIcon} alt="telegram"/>
           </a>
 
-          <LangSwitch lang={lang} setLang={setLang} />
+          <div className={s.switchWrapper}>
+            <LangSwitch lang={lang} setLang={setLang}/>
+          </div>
 
         </div>
-        
+
       </header>
   );
 };
